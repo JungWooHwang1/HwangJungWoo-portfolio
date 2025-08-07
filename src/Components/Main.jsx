715755d88d2778dc22bdc5d14d7ecda1b91559f3
JungWooHwang1 <xhdpc123@naver.com>
2025-08-07 20:46:58 +0900

    파이어베이스 빌드 및 배포 기본 파일형식 구성

---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import app from "../firebase";
+import Header from "./Header";
+import About from "./About";
+
+export default function Main() {
+  console.log("Firebase App:", app); // 연결 확인용 로그
+
+  return (
+    <div className="Container">
+      <div id = "root">
+        <Header />
+        <About />
+
+      </div>
+    </div>
+  );
+}
